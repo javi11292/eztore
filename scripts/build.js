1c1a2254758196e9f32d3bf43d8ce96aa107aea3
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-05-19 23:57:26 +0200

    Mark as sideEffect free

--- scripts/build.js
+++ scripts/build.js
@@ -5,11 +5,8 @@ const options = { stdio: "inherit", shell: true }
 const packageJson = require("../package.json")
 
 fs.removeSync("lib")
-spawnSync("babel src --source-maps --out-dir lib", options)
-spawnSync(
-  "tsc --diagnostics --allowJs --declaration --emitDeclarationOnly  --removeComments  --skipLibCheck  --outDir lib src/index",
-  options
-)
+spawnSync("babel src --out-dir lib", options)
+spawnSync("tsc --diagnostics --allowJs --declaration --emitDeclarationOnly  --removeComments  --skipLibCheck  --outDir lib src/index", options)
 
 delete packageJson.devDependencies
 fs.writeJsonSync("lib/package.json", packageJson)

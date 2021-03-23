ebdc4d09c399310ebe5766eb37c603fbc8df0250
= <=>
2021-03-23 21:29:11 +0100

    Fix type

--- src/index.d.ts
+++ src/index.d.ts
@@ -14,7 +14,7 @@ type Store<Slice> = [
   Dispatch<Slice>,
 ]
 
-export default function getStore<V extends Values, Key extends keyof V>(values: V)
+export function getStore<V extends Values, Key extends keyof V>(values: V)
   : <Subscribe extends boolean = true>(key: Key, subscribe?: Subscribe) => Subscribe extends true
     ? Store<V[Key]>
     : Dispatch<V[Key]>;
